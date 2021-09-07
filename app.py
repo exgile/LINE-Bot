f7a54f30745b8eb21d358186e7ad330501ca0792
exgile <o800920@yahoo.com.tw>
2021-09-08 00:34:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,9 +33,4 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-    print("OK1")
+    line_bot_api.reply_message(event.reply_token, "AAAAA")
